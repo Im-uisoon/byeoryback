5fab61b3797b88ebd7ee38f8a72ff97ce7dac43d
sshoon <dodossh@naver.com>
2026-01-27 22:03:57 +0900

    cors 설정 정리

--- src/main/java/com/project/byeoryback/global/config/WebConfig.java
+++ src/main/java/com/project/byeoryback/global/config/WebConfig.java
@@ -5,15 +5,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173", "https://byeory.up.railway.app",
-                        "http://byeory.up.railway.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
 }
